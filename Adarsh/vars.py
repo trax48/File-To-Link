c691a51df0eba82a0cf38cf8b4987ff739ff30f8
trax48 <raxgaming480@gmail.com>
2025-05-09 08:26:02 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,5 +36,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Rax:amit7001@cluster0.cintttk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '@Ace_Files'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Ace_Files'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
